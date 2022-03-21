d42b4171d2df50706d19a95a9d995a40eef4c8d2
e <test>
2022-03-21 11:32:35 +0300

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+1. Install python3
+
+2. Install dependencies:
+```shell
+pip3 install selenium
+pip3 install click
+```
+
+4. Install driver for chrome browser from http://chromedriver.chromium.org/downloads
+
+5. Create a csv file with routers data:
+```csv
+"95.152.40.204","admin","password"
+```
+
+4. Run:
+```shell
+./reset_dns_zte.py reset --driver-path ~/Downloads/chromedriver_mac64_m1/chromedriver --routers routers.csv --dns 8.8.8.8,1.1.1.1
+```
---
+++ reset_dns_zte.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import click
+import csv
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from loguru import logger
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("-d", "--driver-path", type=click.Path(), help="Chromium driver path")
+@click.option("-r", "--routers", type=click.Path(), help="CSV file containing router data: ip, username, password")
+@click.option("--dns", help="Comma separated list of dns servers: 8.8.8.8,1.1.1.1")
+@click.option("--position", help="Start from line N in router-data file")
+def reset(driver_path: str, routers: str, dns: str, position: int):
+
+    router_data = []
+    with open(routers) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        for row in csv_reader:
+            router_data.append(row)
+
+    srv = Service(driver_path)
+    op = webdriver.ChromeOptions()
+
+    for r in router_data:
+        router_ip, router_username, router_password = r
+        dns_servers = dns.split(",")
+        router_url = f"http://{router_ip}"
+
+        logger.info(f"Processing {router_ip}")
+        driver = webdriver.Chrome(service=srv, options=op)
+        driver.get(router_url)
+
+        # Login
+        username_field = driver.find_element(By.ID, "Frm_Username")
+        password_field = driver.find_element(By.ID, "Frm_Password")
+        username_field.send_keys(router_username)
+        password_field.send_keys(router_password)
+
+        driver.find_element(By.ID, "LoginId").click()
+        logger.info(f"Logged in")
+
+        # Navigate to DNS settings page
+        WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, "LANUrl"))
+        driver.find_element(By.ID, "LANUrl").click()
+
+        WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, "smDns"))
+        driver.find_element(By.ID, "smDns").click()
+
+        WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, "LocalDnsServerBar"))
+        driver.find_element(By.ID, "LocalDnsServerBar").click()
+
+        # Update DNS settings
+        WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, "sub_SerIPAddress10"))
+
+        for row in range(1, 3):
+            logger.info(f"Updating DNS server #{row}")
+            for octet_id in range(4):
+                octet_input = driver.find_element(By.ID, f"sub_SerIPAddress{row}{octet_id}")
+                octet_input.clear()
+                octet_input.send_keys(dns_servers[row-1].split(".")[octet_id])
+
+        driver.find_element(By.ID, "Btn_apply_LocalDnsServer").click()
+        logger.info(f"DNS settings were updated")
+        driver.close()
+
+
+if __name__ == "__main__":
+    cli()
