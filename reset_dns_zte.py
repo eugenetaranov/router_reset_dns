414e03cf35978d4d0dcd188d636435909497f700
e <test>
2022-03-22 13:51:51 +0300

    added config abstracting navigation steps

--- reset_dns_zte.py
+++ reset_dns_zte.py
@@ -2,6 +2,7 @@
 
 import click
 import csv
+import yaml
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
@@ -21,20 +22,23 @@ def cli():
 @click.option("-r", "--routers", type=click.Path(), help="CSV file containing router data: ip, username, password")
 @click.option("--dns", help="Comma separated list of dns servers: 8.8.8.8,1.1.1.1")
 @click.option("--start-from", default=0, help="Start from line N in router-data file")
-def reset(driver_path: str, routers: str, dns: str, start_from: int):
+@click.option("-c", "--config", type=click.Path(), help="Config file, yaml")
+def reset(driver_path: str, routers: str, dns: str, start_from: int, config: str):
     router_data = []
     with open(routers) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         for row in csv_reader:
             router_data.append(row)
 
-    router_data = router_data[start_from:]
+    with open(config, "r") as f:
+        cfg = yaml.safe_load(f)
 
+    router_data = router_data[start_from:]
     srv = Service(driver_path)
     op = webdriver.ChromeOptions()
 
     for r in router_data:
-        router_ip, router_username, router_password = r
+        router_ip, router_username, router_password, model = r
         dns_servers = dns.split(",")
         router_url = f"http://{router_ip}"
 
@@ -48,43 +52,50 @@ def reset(driver_path: str, routers: str, dns: str, start_from: int):
             continue
 
         # Login
-        username_field = driver.find_element(By.ID, "Frm_Username")
-        password_field = driver.find_element(By.ID, "Frm_Password")
-        username_field.send_keys(router_username)
-        password_field.send_keys(router_password)
+        if cfg["routers"][model]["login"]["username"]["type"] == "id":
+            username_field = driver.find_element(By.ID, cfg["routers"][model]["login"]["username"]["location"])
+            username_field.send_keys(router_username)
 
-        driver.find_element(By.ID, "LoginId").click()
+        if cfg["routers"][model]["login"]["password"]["type"] == "id":
+            password_field = driver.find_element(By.ID, cfg["routers"][model]["login"]["password"]["location"])
+            password_field.send_keys(router_password)
+
+        if cfg["routers"][model]["login"]["submit"]["type"] == "id":
+            driver.find_element(By.ID, cfg["routers"][model]["login"]["submit"]["location"]).click()
+
         logger.info(f"Logged in")
 
         # Navigate to DNS settings page
-        try:
-            WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.ID, "LANUrl"))
-        except TimeoutException:
-            driver.close()
-            logger.warning(f"Timed out waiting for LANUrl element, apparently login failed, skipping ...")
-            continue
-        driver.find_element(By.ID, "LANUrl").click()
+        for step in cfg["routers"][model]["steps"]:
+            try:
+                if step["type"] == "id":
+                    WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.ID, step["location"]))
+            except TimeoutException:
+                driver.close()
+                logger.warning(f"Element {step['location']} was not found, skipping router ...")
+                continue
 
-        WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, "smDns"))
-        driver.find_element(By.ID, "smDns").click()
-
-        WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, "LocalDnsServerBar"))
-        driver.find_element(By.ID, "LocalDnsServerBar").click()
+            if step["type"] == "id":
+                driver.find_element(By.ID, step["location"]).click()
 
         # Update DNS settings
-        WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, "sub_SerIPAddress10"))
-
-        for row in range(1, 3):
-            logger.info(f"Updating DNS server #{row}")
-            for octet_id in range(4):
-                octet_input = driver.find_element(By.ID, f"sub_SerIPAddress{row}{octet_id}")
-                octet_input.clear()
-                octet_input.send_keys(dns_servers[row - 1].split(".")[octet_id])
+        logger.info(f"Updating DNS server settings")
+        for dns_idx in range(2):
+            if cfg["routers"][model]["dns"]["split_octets"]:
+                octets = dns_servers[dns_idx].split(".")
+                for idx, loc in enumerate(cfg["routers"][model]["dns"][f"dns_{dns_idx+1}"]["location"]):
+                    if cfg["routers"][model]["dns"][f"dns_{dns_idx+1}"]["type"] == "id":
+                        WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, loc))
+                        octet_input = driver.find_element(By.ID, loc)
+                        octet_input.clear()
+                        octet_input.send_keys(octets[idx])
 
         sleep(1)
-        driver.find_element(By.ID, "Btn_apply_LocalDnsServer").click()
+        if cfg["routers"][model]["dns"]["submit"]["type"] == "id":
+            driver.find_element(By.ID, cfg["routers"][model]["dns"]["submit"]["location"]).click()
+
         logger.info(f"DNS settings were updated")
-        sleep(2)
+        sleep(3)
         driver.close()
 
 
