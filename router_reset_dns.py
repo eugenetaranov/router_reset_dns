230eaa9b4ec77db356344befbcf17c83d0e423a6
e <test>
2022-03-24 13:28:52 +0300

    fix logging

--- router_reset_dns.py
+++ router_reset_dns.py
@@ -278,8 +278,8 @@ def reset(driver_path: str, routers: str, dns: str, start_from: int, config: str
     srv = Service(driver_path)
     op = webdriver.ChromeOptions()
 
-    for router_data in routers_data:
-        logger.info(f"Started router {router_data[0]} {router_data[5]}")
+    for idx, router_data in enumerate(routers_data):
+        logger.info(f"Started {idx} router {router_data[0]} {router_data[5]}")
         model = router_data[5]
         group_model = map_model(cfg=cfg["models"], router_model=model)
         if not group_model:
