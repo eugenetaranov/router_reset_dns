ee089c7734d6986be52f0a1bdc78d2046ecc21f5
e <test>
2022-04-12 12:03:52 +0300

    test passwd update

--- tasks.py
+++ tasks.py
@@ -7,10 +7,13 @@ def test_password_change(ctx, start_from=0):
     if start_from > 2:
         start_from = start_from - 2
 
-    ctx.run(f"./router_reset_dns.py reset \
+    cmd = f"./router_reset_dns.py reset \
   --driver-path {DRIVER_PATH} \
   --routers routers_test_all.csv \
   --new-password 1111 \
   --config config.yaml \
   --start-from {start_from} \
-  --debug")
+  --debug"
+
+    print(cmd)
+    ctx.run(cmd)
